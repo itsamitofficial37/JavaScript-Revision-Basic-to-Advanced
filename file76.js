db1e5586c80e132d42be8cf62a56fbc28253faa0
itsamitofficial37 <amitofficial37@gmail.com>
2023-11-22 14:55:27 +0530

    revision

--- file76.js
+++ file76.js
@@ -1,8 +1,26 @@
 //  New keyword 
 
-function createUser (firstName , age){
+function CreateUser (firstName , lastName, email, age,address) {
     this.firstName = firstName;
-    this.age = age;}
+    this.lastName = lastName;
+    this.email = email;
+    this.age = age;
+    this.address = address;
+}
+CreateUser.prototype.about = function(){
+    return `${this.firstName} is ${this.age} years old`;
+}
 
-const user = new createUser("Amit" , 23)
-console.log(user);
+CreateUser.prototype.is18 = function(){
+    return this.age >= 18;
+}
+
+CreateUser.prototype.sing = function(){
+    return "Jaane woh kaise "
+}
+
+const user1 = new CreateUser('Amit', 'Singh','amitsingh@gmail.com','23',"Neta Ji nagar, Bahadurgarh");
+const user2 = new CreateUser('Upender', 'Yadav','upenderydv8821@gmail.com','24',"Neta Ji nagar, Bahadurgarh");
+const user3 = new  CreateUser('Prince', 'Singh','princesingh@gmail.com','21',"Neta Ji nagar, Bahadurgarh");
+
+console.log(user1);
---
+++ file77.js
@@ -0,0 +1,14 @@
+//  object oriented programming 
+
+class Animals {
+    constructor(name , act){
+        this.name = name;
+        this.act = act;
+    }
+
+}
+
+const animal = new Animals("Dog", "Barking");
+
+console.log(animal)
+
